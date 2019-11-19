dcffcf2816bc7ccdbc5aa6b3b2351d1566c8f8ec
Cammie <cami834j@gmail.com>
2019-11-19 20:46:39 +0100

    Adding building, editing System

--- src/Building.java
+++ src/Building.java
@@ -2,14 +2,48 @@ import java.util.*;
 
 public class Building {
     private String buildingID;
+    private int co2level;
+    private boolean windownsOpen;
+    private ArrayList<String> actuators;
 
-    private List<Actuator> actuators;
-    private List<Sensor> sensors;
 
-    public Building(String buildingID) {
+    public Building(String buildingID, int co2level, boolean windownsOpen, ArrayList<String> actuators) {
         this.buildingID = buildingID;
-        this.actuators = new ArrayList<>();
-        this.sensors = new ArrayList<>();
+        this.co2level = co2level;
+        this.windownsOpen = windownsOpen;
+        this.actuators = actuators;
+    }
+
+    public String getBuildingID() {
+        return buildingID;
+    }
+
+    public void setBuildingID(String buildingID) {
+        this.buildingID = buildingID;
+    }
+
+    public int getCo2level() {
+        return co2level;
+    }
+
+    public void setCo2level(int co2level) {
+        this.co2level = co2level;
+    }
+
+    public boolean isWindownsOpen() {
+        return windownsOpen;
+    }
+
+    public void setWindownsOpen(boolean windownsOpen) {
+        this.windownsOpen = windownsOpen;
+    }
+
+    public ArrayList<String> getActuators() {
+        return actuators;
+    }
+
+    public void setActuators(ArrayList<String> actuators) {
+        this.actuators = actuators;
     }
 
 }
---
+++ src/BuildingSystem.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BuildingSystem {
+private ArrayList<Building> buildings;
+
+public BuildingSystem(){
+    this.buildings = new ArrayList<>();
+    play();
+}
+
+    private void play() {
+        boolean finished = false;
+        Scanner s = new Scanner(System.in);
+        String command;
+        System.out.printf("Welcome to the sensor/actuator system.%nAdd a building by typing 'add building'.%n" +
+                "You can also see a list of all the buildings in the system by typing 'list buildings'");
+        while (!finished) {
+            System.out.println("please type a command");
+            command = s.next().toLowerCase();
+           if(command.equals("quit")) {
+               finished = true;
+               continue;
+           }
+
+           ;
+
+        }
+
+        }
+
+public void addBuilding(){
+
+
+}
+}
